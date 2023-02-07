bdca87d3bae780ab04e5577a0011ec924e3202c5
mederocc <mederocc@gmail.com>
2023-02-07 15:48:36 -0300

    sd

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ require("dotenv").config();
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(`0.0.0.0:${process.env.PORT}`, () => {
-    console.log(process.env.PORT); // eslint-disable-line no-console
+  server.listen(3001, () => {
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
