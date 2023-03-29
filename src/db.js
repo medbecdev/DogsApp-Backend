04b58fb5758563894ae868066b0ec3f69bb07ebf
mederocc <mederocc@gmail.com>
2023-03-29 10:31:55 -0300

    changing env variable names

--- src/db.js
+++ src/db.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { PGUSER, PGPASSWORD, PGHOST } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
+  `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/dogs`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
