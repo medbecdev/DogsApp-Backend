e6a616f88fc9e404ca575d93097ab97054501e02
mederocc <mederocc@gmail.com>
2023-03-29 13:51:12 -0300

    fixing seeder

--- index.js
+++ index.js
@@ -19,11 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const { getApiData } = require("./src/seeder");
+// const { getApiData } = require("./src/seeder");
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  getApiData();
+conn.sync({ alter: true }).then(() => {
+  // getApiData();
   server.listen(3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
